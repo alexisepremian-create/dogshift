5e15e84d4ce71cd44831580ac81cc074d84fa47a
Alexis Epremian <alexis@macbook-pro-de-alexis.home>
2026-01-09 00:20:02 +0100

    fix: trim google oauth env vars

--- dogshift/lib/nextauth.ts
+++ dogshift/lib/nextauth.ts
@@ -144,8 +144,8 @@ export const authOptions: NextAuthOptions = {
     ...(hasGoogleOauth
       ? [
           GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            clientId: googleClientIdTrimmed,
+            clientSecret: googleClientSecretTrimmed,
             allowDangerousEmailAccountLinking: true,
             authorization: {
               params: {
