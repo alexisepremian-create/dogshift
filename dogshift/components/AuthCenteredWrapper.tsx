1ff6c68b4e7b204379dd56ccd6305ea53f29049a
Alexis Epremian <alexis@MacBook-Pro-de-Alexis.local>
2026-01-12 11:04:33 +0100

    style: raise auth cards more

--- dogshift/components/AuthCenteredWrapper.tsx
+++ dogshift/components/AuthCenteredWrapper.tsx
@@ -5,7 +5,7 @@ export default function AuthCenteredWrapper({ children }: { children: React.Reac
         className={
           "flex w-full justify-center px-4" +
           " min-h-[calc(100vh-120px)]" +
-          " pt-[clamp(16px,4vh,40px)]" +
+          " pt-[clamp(8px,2vh,24px)]" +
           " pb-[clamp(24px,6vh,72px)]" +
           " items-start"
         }
