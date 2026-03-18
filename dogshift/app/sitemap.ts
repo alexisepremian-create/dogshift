155eadb32c77e414aaf6928a6ebdae989d0813f7
Alexis Epremian <alexis@MacBook-Pro-de-Alexis.local>
2026-03-18 09:57:09 +0100

    feat: add native sitemap

---
+++ dogshift/app/sitemap.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const lastModified = new Date();
+
+  return [
+    {
+      url: "https://www.dogshift.ch",
+      lastModified,
+    },
+    {
+      url: "https://www.dogshift.ch/devenir-dogsitter",
+      lastModified,
+    },
+    {
+      url: "https://www.dogshift.ch/reserver",
+      lastModified,
+    },
+    {
+      url: "https://www.dogshift.ch/profil",
+      lastModified,
+    },
+  ];
+}
